33b214e3e6e42d659b3430ec4ac4ebda78250cc2
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-04 14:20:32 -0300

    Adicionado Desafio02.py

---
+++ Desafio02.py
@@ -0,0 +1,9 @@
+"""
+Crie um script que leia o dia, mes e ano
+do nascimento de uma pessoa e mostre uma
+mensagem com a data formatada.
+"""
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes,'de', ano,'. Correto?')
