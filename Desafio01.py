7954bb677222f54d134f443820e3633d0670fa95
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-04 14:06:39 -0300

    Atualização do README.

---
+++ Desafio01.py
@@ -0,0 +1,6 @@
+"""
+Crie um script que pergunte o nome de uma pessoa
+e escreva uma mensagem de boas vindas.
+"""
+nome = input('Qual o seu nome? ')
+print('Olá', nome, '! Prazer em te conhecer!!')
