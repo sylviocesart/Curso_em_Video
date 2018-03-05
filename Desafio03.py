a7d74592ab0f376f36a8a8b1da1f978515c864d4
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-04 22:19:44 -0300

    Adicionado Desafio03.py

---
+++ Desafio03.py
@@ -0,0 +1,11 @@
+"""
+Crie um script que leia dois números e tente
+mostrar a soma entre eles
+"""
+num1 = input('Primeiro número: ')
+num2 = input('Segundo número: ')
+print('A soma é',num1 + num2)  # O resultado foi a junção dos números.
+
+# Agora, fazendo a soma realmente dos números
+print('A soma é', int(num1) + int(num2)) # Aqui o resultado é num1+num2
+print('A soma é', int(num1+num2)) # Já aqui, o resultado é a junção dos números
