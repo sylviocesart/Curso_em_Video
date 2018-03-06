0090a80197e788a256f5a5ebe4c10011711d9966
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-06 09:07:17 -0300

    Aula 06 atualizada.

--- Aula06/aula06a.py
+++ Aula06/aula06a.py
@@ -6,3 +6,9 @@ print(type(n1))
 
 n2 = int(input('Digite um número: '))
 print(type(n2))
+
+# Somando dois números e utilizando o novo formato para o python3
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
