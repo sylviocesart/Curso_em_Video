76d7030c4cf2013bb00352518dbf320343bb6d42
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-06 09:08:43 -0300

    Aula 06b adicionada. Teste de variáveis.

---
+++ Aula06/aula06b.py
@@ -0,0 +1,14 @@
+# Fazendo vários testes com o resultado de uma variável
+# n = input('Digite algo: ')
+
+# Testando para ver se o valor é numérico. Caso seja numerico, True, caso
+# não, será impresso False
+# print(n.isnumeric())
+
+# Testando se é letra
+# l = input('Digite algo: ')
+# print(l.isalpha())
+
+# Testando se é alfanumérico
+a = input('Digite algo: ')
+print(a.isalnum())
