9965f1b1b57b9a75d7dc85baff496364881dc6e2
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-06 22:07:17 -0300

    Adcionado Desafio01.py da aula06.

---
+++ Aula06/Desafio01.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que leia dois números e mostre a
+soma entre eles. Utilizando tipos primitivos
+"""
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print('A soma entre, {} e {}, vale {}'.format(n1, n2, soma))
