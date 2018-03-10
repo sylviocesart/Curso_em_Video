1772ebc94e04560db50503948ff8949b7bff614f
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-10 20:41:23 -0300

    Atualizado Desafio06

--- Desafios/Desafio06.py
+++ Desafios/Desafio06.py
@@ -5,6 +5,8 @@ o seu dobro, triplo e raiz quadrada
 n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
-rz = n ** (1/2)
+# rz = n ** (1/2)
+# Raiz quadrada usando a funcao pow
+rz = pow(n, (1/2))
 print('O número digitado foi: {}. O dobro dele é {}'.format(n, d), end=' ')
-print('e o triplo é {} e a raiz quadrada é {}'.format(t, rz))
+print('e o triplo é {} e a raiz quadrada é {:.2f}'.format(t, rz))
