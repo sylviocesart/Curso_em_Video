adb113a716237a66396e4df3ae13e8d980dcdbc3
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-03-11 10:51:29 -0300

    Alterado tipo de int para float no Desafio08

--- Desafios/Desafio08.py
+++ Desafios/Desafio08.py
@@ -3,7 +3,7 @@ Escreva um programa que leia um valor
 em metros e o exiba convertido em centimetros
 e em milimetros
 """
-n = int(input('Digite um valor em metros: '))
+n = float(input('Digite um valor em metros: '))
 cent = n * 100
 mili = n * 1000
 print('O valor de {} metros, corresponde a {} centimetros e em {} milimetros'.format(n, cent, mili))
